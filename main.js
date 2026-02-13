8ef757d349fe59f5f4d0a26e57873c595518ef3a
jayanth-14 <jayanthpetchetii@gmail.com>
2026-02-13 08:24:57 +0530

    implements taking input from the user and updating the board with black disc.
    * Iterates the entire flow in infinite loop.
    * Takes the user input in format (row-column).
    * Parses the input into row and column.
    * computes the row and column to use in array.
    * computes the cell number using computed row and column.
    * updates the board at the cell number to black disc.

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import { createBoard, generateBoardStr } from "./src/board.js";
 const main = () => {
   const board = createBoard();
-  const boardStr = generateBoardStr(board);
-  console.log(boardStr);
+  while (true) {
+    const boardStr = generateBoardStr(board);
+    console.log(boardStr);
+    const input = prompt("Enter The cell number in format (row-column):");
+    const [row, column] = input.split("-");
+    const computedRow = Number(row) - 1;
+    const computedColumn = Number(column) - 1;
+    const cellNumber = (computedRow * 8) + computedColumn;
+    board[cellNumber] = "⚫️";
+  }
 };
 
 main();
